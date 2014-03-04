fcef9bb9b672e1ec8abc3e13db7775ea415c0f8d
hebun <itung73@gmail.com>
2014-03-04 09:57:06 +0200

    commit all

---
+++ src/HelloBean.java
@@ -0,0 +1,22 @@
+
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
