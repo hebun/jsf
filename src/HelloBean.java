08823ef5ad9089ea815aa0475bf9685ea0f56800
hebun <itung73@gmail.com>
2014-03-05 18:57:16 +0200

    zzzz

--- src/HelloBean.java
+++ src/HelloBean.java
@@ -1,5 +1,6 @@
-
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -8,7 +9,43 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class HelloBean implements Serializable {
+
+	private boolean isDebug;
+	public String email;
 	private String name;
+	private List<String> list = null;
+
+	public HelloBean() {
+
+		this.list = new ArrayList<String>();
+	}
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
+	private int no = 0;
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		System.out.println(no + " sent to us");
+		this.no = no;
+	}
+
+	public boolean getIsDebug() {
+		return isDebug;
+	}
+
+	public void setIsDebug(boolean isDebug) {
+		this.isDebug = isDebug;
+	}
 
 	public String getName() {
 		return name;
@@ -19,9 +56,29 @@ public class HelloBean implements Serializable {
 	}
 
 	public String clicked() {
+		if (this.list == null) {
+
+		}
 		setName("clicked the button");
-		System.out.println("clicked");
+		System.out.println("..");
+		setIsDebug(!getIsDebug());
+
+		setEmail("updated via ajax");
+
 		return "true";
 
+	}
+
+	public String listen() {
+
+		return "from listen method";
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
