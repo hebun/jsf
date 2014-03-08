43b8e9031500ac592897290df761420466419a43
hebun <itung73@gmail.com>
2014-03-08 12:10:24 +0200

    login added

--- src/Login.java
+++ src/Login.java
@@ -1,21 +1,28 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.primefaces.context.RequestContext;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @SessionScoped
-public class HelloBean implements Serializable {
+public class Login implements Serializable {
 
 	private boolean isDebug;
 	public String email;
 	private String name;
 	private List<String> list = null;
 
-	public HelloBean() {
+	public Login() {
 
 		this.list = new ArrayList<String>();
 	}
@@ -86,5 +93,37 @@ public class HelloBean implements Serializable {
 
 		System.out.println("com action called");
 		return "okay";
+	}
+
+	public static String getResourceBundleString(String resourceBundleName,
+			String resourceBundleKey) throws MissingResourceException {
+
+		ResourceBundle bundle = ResourceBundle.getBundle("util.hello",
+				FacesContext.getCurrentInstance().getViewRoot().getLocale());
+		
+		String message = bundle.getString(resourceBundleKey);
+		
+		return message;
+	}
+
+	public void login(ActionEvent actionEvent) {
+		RequestContext context = RequestContext.getCurrentInstance();
+		FacesMessage msg = null;
+		boolean loggedIn = false;
+
+		if (this.name != null && name.equals("admin") && email != null
+				&& email.equals("admin")) {
+			loggedIn = true;
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", name);
+		} else {
+			loggedIn = false;
+
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					getResourceBundleString("util.hello", "invalidCred"),
+					"Invalid credentials");
+		}
+
+		FacesContext.getCurrentInstance().addMessage("auth", msg);
+		context.addCallbackParam("loggedIn", loggedIn);
 	}
 }
